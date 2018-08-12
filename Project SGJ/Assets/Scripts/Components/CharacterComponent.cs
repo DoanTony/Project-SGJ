a572e6b096cfc17f5b0d20ddebc94ce0c5ea9f01
Tony Doan <doantony90@gmail.com>
2018-08-12 00:12:16 -0400

    Added steal mechanic

--- Project SGJ/Assets/Scripts/Components/CharacterComponent.cs
+++ Project SGJ/Assets/Scripts/Components/CharacterComponent.cs
@@ -7,17 +7,46 @@ public class CharacterComponent : MonoBehaviour
     public SpriteRenderer transporter;
     public bool hasTransporter;
 
+    private PlayerControllerComponent pcc;
+
+    private void Start()
+    {
+        pcc = GetComponent<PlayerControllerComponent>();
+    }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.transform.tag == "Obstacle" || collision.transform.tag == "Player")
+        if (collision.transform.tag == "Obstacle")
         {
             if (hasTransporter)
             {
-                Instantiate(characterObject.transmiterPrefab, this.transform.position, Quaternion.Euler(Vector3.zero));
-                hasTransporter = false;
+                DropTransporter();
             }
         }
+        else if (collision.transform.tag == "Player")
+        {
+            PlayerControllerComponent otherPcc = collision.transform.GetComponent<PlayerControllerComponent>();
+            CharacterComponent otherCc = collision.transform.GetComponent<CharacterComponent>();
+            if (hasTransporter)
+            {
+                if (otherPcc.isDashing && pcc.isDashing)
+                {
+                    DropTransporter();
+                }
+                else if(otherPcc.isDashing && !pcc.isDashing)
+                {
+                    hasTransporter = false;
+                    otherCc.hasTransporter = true;
+                }
+            }
+        }
+    }
+
+    private void DropTransporter()
+    {
+        Instantiate(characterObject.transmiterPrefab, this.transform.position, Quaternion.Euler(Vector3.zero));
+        hasTransporter = false;
     }
 
 }
